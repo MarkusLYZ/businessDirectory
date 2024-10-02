b64153c255e5553052d2ba58baa3c46bdabd05ab
MarkusLYZ <markoos203@gmail.com>
2024-10-02 16:49:19 +0800

    slider

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,11 +1,13 @@
 import { View, Text } from "react-native";
 import React from "react";
 import Header from "../../components/Home/Header";
+import Slider from "../../components/Home/Slider";
 
 export default function home() {
   return (
     <View style={{ padding: 20, marginTop: 20 }}>
-      <Header></Header>
+      <Header/>
+      <Slider/>
     </View>
   );
 }
---
+++ components/Home/Slider.jsx
@@ -0,0 +1,45 @@
+import { View, Text, FlatList, Image, StyleSheet, Dimensions } from "react-native";
+import React, { useEffect, useState } from "react";
+import { db } from "./../../config/FirebaseConfig";
+import { collection, getDocs } from "firebase/firestore";
+
+export default function Slider() {
+  const [sliderList, setSliderList] = useState([]);
+  useEffect(() => {
+    GetSlider();
+  }, []);
+  const GetSlider = async () => {
+    setSliderList([]);
+    const snapshot = await getDocs(collection(db, "Sliders"));
+    snapshot.forEach((doc) => {
+      console.log(doc.data());
+      setSliderList((sliderList) => [...sliderList, doc.data()]);
+    });
+  };
+  return (
+    <View style={{marginTop:15}}>
+      <FlatList
+        data={sliderList}
+        horizontal
+        showsHorizontalScrollIndicator = {false}
+        renderItem={({ item, index }) => (
+          <View>
+            <Image
+              source={{ uri: item?.imageUrl }}
+              style={styles?.sliderImage}
+            />
+          </View>
+        )}
+      ></FlatList>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  sliderImage: {
+    width: Dimensions.get('screen').width*0.9,
+    height: 170,
+    borderRadius:15,
+    marginRight:15
+  },
+});
