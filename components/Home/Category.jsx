b37561e31aed44b68657027096d579b3875c5f74
MarkusLYZ <markoos203@gmail.com>
2024-10-07 11:11:01 +0800

    petListItemsUpdate

--- components/Home/Category.jsx
+++ components/Home/Category.jsx
@@ -5,7 +5,7 @@ import { db } from "./../../config/FirebaseConfig";
 import Colors from "../../constants/Colors";
 import { TouchableOpacity } from "react-native";
 
-export default function Category() {
+export default function Category({category}) {
   const [categoryList, setCategoryList] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState("Dogs");
   useEffect(() => {
@@ -32,7 +32,13 @@ export default function Category() {
         data={categoryList}
         numColumns={4}
         renderItem={({ item, index }) => (
-          <TouchableOpacity onPress={()=>setSelectedCategory(item.name)} style={{ flex: 1 }}>
+          <TouchableOpacity
+            onPress={() => {
+              setSelectedCategory(item.name);
+              category(item.name);
+            }}
+            style={{ flex: 1 }}
+          >
             <View
               style={[
                 styles.container,
