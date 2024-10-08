629c39339b15b641eebccaaf610b2e5aeb159afe
MarkusLYZ <markoos203@gmail.com>
2024-10-09 00:27:32 +0800

    petDetailsButton

--- components/Home/PetListItem.jsx
+++ components/Home/PetListItem.jsx
@@ -1,10 +1,18 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 import React from "react";
 import Colors from "../../constants/Colors";
+import { useRouter } from "expo-router";
 
 export default function PetListItem({ pet }) {
+  const router = useRouter();
   return (
-    <View
+    <TouchableOpacity
+      onPress={() =>
+        router.push({
+          pathname: "/pet-details",
+          params: pet,
+        })
+      }
       style={{
         padding: 10,
         marginRight: 15,
@@ -24,12 +32,29 @@ export default function PetListItem({ pet }) {
       <Text style={{ fontFamily: "outfit-medium", fontSize: 18 }}>
         {pet.name}
       </Text>
-      <View style={{display:'flex', flexDirection:'row', justifyContent:'space-between'}}>
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
         <Text style={{ color: Colors.GRAY, fontFamily: "outfit" }}>
           {pet?.breed}
         </Text>
-        <Text style={{fontFamily:'outfit', color: Colors.PRIMARY, paddingHorizontal:6, borderRadius:10, backgroundColor:Colors.LIGHT_PRIMARY,fontSize:11}}>{pet.age} Yrs</Text>
+        <Text
+          style={{
+            fontFamily: "outfit",
+            color: Colors.PRIMARY,
+            paddingHorizontal: 6,
+            borderRadius: 10,
+            backgroundColor: Colors.LIGHT_PRIMARY,
+            fontSize: 11,
+          }}
+        >
+          {pet.age} Yrs
+        </Text>
       </View>
-    </View>
+    </TouchableOpacity>
   );
 }
