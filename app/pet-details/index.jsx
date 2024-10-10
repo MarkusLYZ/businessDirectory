3b3304b1f1680b3e031c85da5de2200ec49ce71f
MarkusLYZ <markoos203@gmail.com>
2024-10-10 15:38:59 +0800

    petSubInfo

--- app/pet-details/index.jsx
+++ app/pet-details/index.jsx
@@ -1,10 +1,29 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React, { useEffect } from "react";
+import { useLocalSearchParams, useNavigation } from "expo-router";
+import PetInfo from "../../components/PetDetails/PetInfo";
+import PetSubInfo from "../../components/PetDetails/PetSubInfo";
 
-export default function index() {
+export default function PetDetails() {
+  const pet = useLocalSearchParams();
+  const navigation = useNavigation();
+  useEffect(() => {
+    navigation.setOptions({
+      headerTransparent: true,
+      headerTitle: "",
+    });
+  }, []);
   return (
     <View>
-      <Text>Pet Details</Text>
+      {/* Pet Info */}
+      <PetInfo pet={pet} />
+      {/* Pet Properties  */}
+      <PetSubInfo pet={pet} />
+      {/* about  */}
+
+      {/* owner details  */}
+
+      {/* adopt me button */}
     </View>
-  )
+  );
 }
